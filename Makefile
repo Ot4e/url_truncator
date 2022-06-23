1c9758fb25075af49784067e2862a9bd208c1f89
Serhii Hetman <bat_ss@yahoo.com>
2022-06-23 18:33:56 +0300

    init project structure

---
+++ Makefile
@@ -0,0 +1,2 @@
+init:
+	cd truncat && sqlite3 truncut.sqlite < init.sql
---
+++ truncat/init.sql
@@ -0,0 +1,35 @@
+CREATE TABLE users (
+    ID     INTEGER  PRIMARY KEY AUTOINCREMENT,
+    login  TEXT     NOT NULL
+                    UNIQUE,
+    passw  TEXT     NOT NULL,
+    reg_at DATETIME DEFAULT (CURRENT_TIMESTAMP) 
+);
+
+CREATE TABLE source (
+    truncat   TEXT     UNIQUE
+                       NOT NULL,
+    input     TEXT     NOT NULL,
+    make_at   DATETIME DEFAULT (CURRENT_TIMESTAMP),
+    owner     INTEGER  REFERENCES users (ID) ON DELETE SET NULL
+                                             ON UPDATE NO ACTION,
+    source_gr INTEGER  REFERENCES gr (id) ON DELETE SET NULL
+                                          ON UPDATE NO ACTION
+);
+
+CREATE TABLE log (
+    ID     INTEGER  PRIMARY KEY,
+    what   INTEGER  REFERENCES source (id) ON DELETE CASCADE
+                                           ON UPDATE NO ACTION,
+    use_at DATETIME DEFAULT (CURRENT_TIMESTAMP),
+    who    INTEGER  REFERENCES users (ID) ON DELETE SET NULL
+                                          ON UPDATE NO ACTION
+);
+
+CREATE TABLE gr (
+    id       INTEGER PRIMARY KEY,
+    gr_name  TEXT    NOT NULL
+                     UNIQUE,
+    gr_owner INTEGER REFERENCES users (ID) ON DELETE CASCADE
+                                           ON UPDATE NO ACTION
+);
