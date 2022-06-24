c3aac1d9d39122320c17b9496325a3a8b4c7fa47
Serhii Hetman <bat_ss@yahoo.com>
2022-06-24 13:08:02 +0300

    application structure

--- truncat/appy.py
+++ truncat/appy.py
@@ -1,3 +1,4 @@
+from crypt import methods
 from flask import Flask
 
 app = Flask(__name__)
@@ -8,6 +9,36 @@ DATABASE = "truncat.sqlite"
 @app.route("/")
 def frontpage():
     return "Ok"
+
+
+@app.route("/about")
+def aboutpage():
+    return "Ok"
+
+
+@app.route("/contact")
+def contactpage():
+    return "Ok"
+
+
+@app.route("/signup")
+def sognuppage():
+    pass
+
+
+@app.route("/login")
+def loginpage():
+    pass
+
+
+@app.route("/logout")
+def logoutpage():
+    pass
+
+
+@app.route("/<urlid>", method=["GET", "POST"], endpoint="redirect")
+def redirectpage():
+    pass
 
 
 if __name__ == "__main__":
