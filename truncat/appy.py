ad93bf05a26475b4709676cbafd5c3ab5dccf376
Serhii Hetman <bat_ss@yahoo.com>
2022-06-25 15:29:10 +0300

    login/singup work with db

--- truncat/appy.py
+++ truncat/appy.py
@@ -1,20 +1,32 @@
-from flask import Flask, redirect, render_template, session, g
-from forms import LoginForm
+import sqlite3
+import bcrypt
+from flask import Flask, redirect, render_template, session, g, flash
+from forms import LoginForm, SignupForm
 
 app = Flask(__name__)
-app.secret_key = "secret"
+app.secret_key = "abrvalg"
 
 DATABASE = "truncat.sqlite"
 
 
 @app.before_request
 def global_vars():
+    """Установка глобальных переменных для работы с залогиненным пользователем"""
     if session.get("logged_in"):
         g.logged = True
 
 
+def db():
+    """Подключение к базе данных (БД)"""
+    if not hasattr(g, "db"):
+        g.db = sqlite3.connect(DATABASE)
+        g.db.row_factory = sqlite3.Row
+    return g.db
+
+
 @app.route("/")
 def frontpage():
+    """Отображение главной страницы"""
     return render_template("index.html")
 
 
@@ -28,18 +40,58 @@ def contactpage():
     return "Ok"
 
 
-@app.route("/signup")
+@app.route("/signup", endpoint="singup", methods=["GET", "POST"])
 def signuppage():
-    pass
+    """Отображение и обработка формы регистрации"""
+    form = SignupForm()
+    if form.validate_on_submit():
+        registred_name = db().execute("SELECT login FROM users").fetchall()
+        if form.password.data == form.password2.data:
+            # проверка ниличия такого же имени в БД
+            for i in registred_name:
+                if form.name.data == i[0]:
+                    flash("Это имя уже используется, выбирите другое")
+                    return render_template("signup.html", form=form)
+            # хэширование пароля для безопасного хранения в БД
+            salt = bcrypt.gensalt()
+            password = form.password.data.encode("utf-8")
+            password_hash = bcrypt.hashpw(password, salt)
+            # добавляем нового пользователя в БД
+            db().execute(
+                "INSERT INTO users (login, passw) VALUES (? , ?)",
+                (form.name.data, password_hash),
+            )
+            db().commit()
+            # где выводить это сообщение?
+            flash("Вы успешно зарегистрированы и вошли в свой новый профиль")
+            session["logged_in"] = True
+            # имя залогиненного пользователя
+            g.logname = form.name.data
+            return redirect("/")
+        flash("Введенные пароли не совпадают, попробуйте еще раз")
+    return render_template("signup.html", form=form)
 
 
 @app.route("/login", endpoint="login", methods=["GET", "POST"])
 def loginpage():
+    """Вход зарегистрированного пользователя"""
     form = LoginForm()
     if form.validate_on_submit():
-        # if form.name.data == logname and form.password.data == lognamepassw:
-        session["logged_in"] = True
-        return redirect("/truncate")
+        db_passw = (
+            db()
+            .execute("SELECT passw FROM users WHERE login=(?)", (form.name.data,))
+            .fetchone()
+        )
+        # сверка пароля из БД с введенным для данного пользователя
+        if db_passw:
+            log_passw = form.password.data.encode("utf-8")
+            if bcrypt.checkpw(log_passw, db_passw["passw"]):
+                g.logname = form.name.data
+                session["logged_in"] = True
+                return redirect("/")
+            flash("Введен неправильный пароль")
+        else:
+            flash("Пользователь с таким именем не найден")
     return render_template("login.html", form=form)
 
 
@@ -49,7 +101,7 @@ def logoutpage():
     return redirect("/")
 
 
-@app.route("/<urlid>", methods=["GET", "POST"], endpoint="redirect")
+@app.route("/<urlid>", methods=["GET", "POST"], endpoint="redirection")
 def redirectpage():
     pass
 
@@ -64,5 +116,6 @@ def truncatepage():
     pass
 
 
+# главная функция
 if __name__ == "__main__":
     app.run()
--- truncat/forms.py
+++ truncat/forms.py
@@ -1,8 +1,12 @@
+"""Формы для сайта"""
+
 from flask_wtf import FlaskForm
 from wtforms import StringField, validators, PasswordField
 
 
 class LoginForm(FlaskForm):
+    """Форма входа для зарегистрированного пользователя"""
+
     name = StringField(
         "name",
         validators=[validators.DataRequired()],
@@ -12,4 +16,24 @@ class LoginForm(FlaskForm):
         "password",
         validators=[validators.DataRequired()],
         render_kw={"placeholder": "Ваш пароль *", "class": "form-control"},
+    )
+
+
+class SignupForm(FlaskForm):
+    """Форма регистрации нового пользователя"""
+
+    name = StringField(
+        "name",
+        validators=[validators.DataRequired()],
+        render_kw={"placeholder": "Ваше имя *", "class": "form-control"},
+    )
+    password = PasswordField(
+        "password",
+        validators=[validators.DataRequired()],
+        render_kw={"placeholder": "Ваш пароль *", "class": "form-control"},
+    )
+    password2 = PasswordField(
+        "password2",
+        validators=[validators.DataRequired()],
+        render_kw={"placeholder": "еще раз Ваш пароль *", "class": "form-control"},
     )
